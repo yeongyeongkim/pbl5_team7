2edffe663c105e21a2c1354f639dee4a5c61f64a
yeongyeongkim <developerjayl@gmail.com>
2021-04-01 23:31:24 +0900

    #7 spring boot init

---
+++ backend/src/main/java/kr/ac/swu/pbl5/Pbl5Application.java
@@ -0,0 +1,13 @@
+package kr.ac.swu.pbl5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pbl5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pbl5Application.class, args);
+	}
+
+}
