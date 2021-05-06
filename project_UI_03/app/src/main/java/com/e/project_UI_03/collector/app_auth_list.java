b5f88f824fc0fab53b902b54dbac122ec6f1d2f5
song-jin-su <jinsu811@naver.com>
2021-05-06 14:16:45 +0900

    permission info in text

--- project_UI_03/app/src/main/java/com/e/project_UI_03/collector/app_auth_list.java
+++ project_UI_03/app/src/main/java/com/e/project_UI_03/collector/app_auth_list.java
@@ -14,9 +14,10 @@ import android.widget.TextView;
 import com.e.project_UI_03.R;
 import com.e.project_UI_03.UI.auth_setting_popup;
 
-public class app_auth_list extends Activity implements OnClickListener {
+public class app_auth_list extends Activity {
     TextView appLabel;
     ImageView appIcon;
+    TextView permissions;
     PackageInfo packageInfo;
 
     @Override
@@ -29,21 +30,39 @@ public class app_auth_list extends Activity implements OnClickListener {
         packageInfo = appData.getPackageInfo();
         setValues();
 
-        View View = (View) findViewById(R.id.popup_auth_setting);
-        TextView appAuthList = (TextView) findViewById(R.id.txt_authName);
-        appAuthList.setOnClickListener(this);
+        // View View = (View) findViewById(R.id.popup_auth_setting);
+        // TextView appAuthList = (TextView) findViewById(R.id.txt_authName);
+        // appAuthList.setOnClickListener(this);
     }
 
     private void findViewsById() {
         appLabel = (TextView) findViewById(R.id.authList_appName);
         appIcon = (ImageView) findViewById(R.id.authList_appIcon);
+        permissions = (TextView) findViewById(R.id.req_permission);
     }
 
     private void setValues() {
         appLabel.setText(getPackageManager().getApplicationLabel(packageInfo.applicationInfo));
         appIcon.setImageDrawable(getPackageManager().getApplicationIcon(packageInfo.applicationInfo));
+
+        // uses-permission
+        if (packageInfo.requestedPermissions != null)
+            permissions
+                    .setText(getPermissions(packageInfo.requestedPermissions));
+        else
+            permissions.setText("NONE");
     }
 
+    private String getPermissions(String[] requestedPermissions) {
+        String permission = "";
+        for (int i = 0; i < requestedPermissions.length; i++) {
+            permission = permission + requestedPermissions[i] + ",\n";
+        }
+        return permission;
+    }
+
+    /*
+    // RadioClick - delete needed
     public void RadioClick(View v) {
         final String [] items = {"30%", "50%", "70%", "허용/거부"};
         new AlertDialog.Builder(this).setTitle("비율 선택").setSingleChoiceItems(items, -1, new DialogInterface.OnClickListener() {
@@ -68,5 +87,11 @@ public class app_auth_list extends Activity implements OnClickListener {
                 break;
         }
     }
+    */
+
 
 }
+
+//PackageManager pm = context.getPackageManager();
+//PermissionInfo info = pm.getPermissionInfo("PERMISSION_NAME", OPTIONAL_FLAGS);
+//int drawableResource = info.icon;
